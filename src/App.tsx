45276e2d9a6860ff0777007fa2acdb5ab6ba153a
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-07-09 09:38:02 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import { Container, Grid } from '@material-ui/core'
 
-function App() {
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  image: {
+    width: '100%',
+    height: 'auto',
+  },
+  videoContainer: {
+    position: 'relative',
+    paddingBottom: '31.25%', // Mantener una relación de aspecto 16:9 (para videos de YouTube)
+    paddingTop: '30px',
+    height: 0,
+    overflow: 'hidden',
+  },
+  videoIframe: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+}))
+
+const App: React.FC = () => {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <img src="/img/1.png" alt="Imagen 1" className={classes.image} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img src="/img/2.png" alt="Imagen 2" className={classes.image} />
+        </Grid>
+        <Grid item xs={12}>
+          <div className={classes.videoContainer}>
+            <iframe
+              className={classes.videoIframe}
+              width="480" // Ajustar el ancho del video según tus necesidades
+              height="270" // Ajustar el alto del video según tus necesidades
+              src="https://www.youtube.com/embed/eTGu0CZs0r0"
+              title="YouTube video"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
-export default App;
+export default App
