6520f3516f4577186e0bb6b0db42219ca244b80a
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-07-12 20:09:52 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
-import { BrowserRouter } from 'react-router-dom'
+import { Container, Button, Typography } from '@material-ui/core'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import { BrowserRouter, Link } from 'react-router-dom'
 import { AppRouter } from './router'
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    flexDirection: 'column',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
 const App: React.FC = () => {
+  const classes = useStyles()
+
   return (
-    <Container>
+    <Container className={classes.root}>
       <BrowserRouter>
         <AppRouter />
+        <Typography variant="h4" className={classes.title}>
+          Welcome to My App
+        </Typography>
+        <Button
+          component={Link}
+          to="/home"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Go to Home
+        </Button>
       </BrowserRouter>
     </Container>
   )
 }
+
 export default App
