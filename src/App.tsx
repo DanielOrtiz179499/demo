128a2146386b02293d0223b9f80ef322d631c2cc
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-07-12 19:50:49 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import React from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
-import { Button, Container, Grid } from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
 
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh',
-  },
-  image: {
-    width: '100%',
-    height: 'auto',
-  },
-  videoContainer: {
-    position: 'relative',
-    paddingBottom: '31.25%', // Mantener una relación de aspecto 16:9 (para videos de YouTube)
-    paddingTop: '30px',
-    height: 0,
-    overflow: 'hidden',
-  },
-  videoIframe: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-  },
-}))
-
 const App: React.FC = () => {
-  const classes = useStyles()
-
   return (
-    <Container className={classes.container}>
+    <Container>
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
