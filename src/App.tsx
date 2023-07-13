2054651fd29942e4a95086a5863dbf63d72d055d
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-07-12 19:51:52 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { makeStyles, Theme } from '@material-ui/core/styles'
 import { Container } from '@material-ui/core'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
